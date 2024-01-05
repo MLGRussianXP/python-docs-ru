5e08faca47e4fefacd37e6f5795d74b58f7540e9
MLGRussianXP <daniilkoles47@gmail.com>
2024-01-05 17:38:01 +0600

    Fix generate_tx_config.bash

--- .scripts/generate_tx_config.bash
+++ .scripts/generate_tx_config.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# run from the .scripts directory
+
 cd ..
 mkdir -p .tx
 touch .tx/config
 
 cd ..
 sed cpython/Doc/locales/.tx/config -e "s|file_filter            = ./<lang>/LC_MESSAGES/|trans.ru               = |; s|= pot/|= ../cpython/Doc/locales/pot/|;" > python-docs-ru/.tx/config
+sed -i -e "/trans.ru/ s/trans.ru\(.*\)/&\nfile_filter\1/; s/\(file_filter\)    /\1 /" python-docs-ru/.tx/config
